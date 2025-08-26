f5d03f418e9d9da5739063a09c45bfb840b8cef2
henryhenged <roniynoam@gmail.com>
2025-08-26 19:27:07 +0300

    Added shader class

---
+++ Rally/src/Rally/Renderer/Shader.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+namespace Rally {
+
+	class Shader
+	{
+	public:
+		Shader(std::string& vertexSrc, std::string& fragmentSrc);
+		~Shader();
+
+		void Bind() const;
+		void Unbind() const;
+	private:
+		uint32_t m_RendererID;
+	public:
+
+	};
+}
